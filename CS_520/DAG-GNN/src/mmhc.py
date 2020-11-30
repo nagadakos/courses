8c65dc06a12956168e31e360ea6f426a164aa348
Nikolaos Agadakos <nagadakos@DESKTOP-6N0JT9I.localdomain>
2020-11-30 16:13:21 -0500

    Added mmhc for causal structure learning

---
+++ CS_520/DAG-GNN/src/mmhc.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from pgmpy.estimators import HillClimbSearch, BicScore, K2Score
+import os
+
+
+
+def to_adjascency_mat(edges, numOfNodes):
+    
+    m = np.zeros((numOfNodes, numOfNodes))
+    
+    for e in edges:
+        i, j = e[0], e[1]
+        m[i,j] = 1
+    return m
+
+
+def main():
+    dataFile = '../Data/Sachs/1. cd3cd28.xls'
+    #print(os.getcwd())
+    df = pd.read_excel(dataFile)
+    df.columns = [i for i in range(1, 12)] # REname columns to numbers for easy of use and readability
+    #print(df)
+
+
+
+    est = HillClimbSearch(df, scoring_method=K2Score(df),)
+    best_model = est.estimate( max_iter = 7)
+
+    print(best_model.edges())
+
+    m = to_adjascency_mat(best_model.edges(), 11)
+    saveMatFile = './Results/mmhc_sachs_data_1.npy'
+    np.save(saveMatFile, m)
+    print(m)
+
+if __name__ == "__main__":
+    main()
