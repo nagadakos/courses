c7889cf2fc9d21cf276c340616a95a0f8fb185c3
Nikolaos Agadakos <nagadakos@DESKTOP-6N0JT9I.localdomain>
2020-12-11 11:39:21 -0500

    Updated mmhc

--- CS_520/DAG-GNN/src/mmhc.py
+++ CS_520/DAG-GNN/src/mmhc.py
@@ -17,10 +17,24 @@ def to_adjascency_mat(edges, numOfNodes):
 
 
 # =====================================================================================
-dataFile = '../Data/Sachs/1. cd3cd28.xls'
+# Crate target filename and Load data
+#dataFile = '../Data/Sachs/1. cd3cd28.xls'
+origFolder = 'Small_Datasets'
+genProc = 'd5_Gauss_01'
+dataFile = os.path.join('..', 'Data', origFolder, genProc, 'data.npy')
+fileType = dataFile.split('.')[-1]
+# Use appropriate laoding procedure according to file type
+if fileType == 'xls':
+    df = pd.read_excel(dataFile)
+elif fileType == 'npy' or fileType == 'txt':
+    data = np.load(dataFile)
+    df = pd.DataFrame(data).astype(np.float32)
 
-df = pd.read_excel(dataFile)
-df.columns = [i for i in range(0, 11)] # REname columns to numbers for easy of use and readability
+# Construct columns and normalize them for convergence speed
+df.columns = [i for i in range(0, df.shape[1])] # REname columns to numbers for easy of use and readability
+#normalized_df=((df-df.min())/(df.max()-df.min())).astype(np.float32)
+#print(normalized_df, df.max())
+print(df)
 
 
 # RUNN MMHC on data
@@ -29,8 +43,10 @@ best_model = est.estimate( max_iter = 500)
 
 # Turn resulting edges to numpy for storage.
 m = to_adjascency_mat(best_model.edges(), 11)
-saveFileBase = './Results/mmhc_sachs_data_1'
+#saveFileBase = './Results/mmhc_sachs_data_1'
+saveFileBase = './Results
+saveFile = os.path.join(saveFileBase, '_'.join(('mmhc', genProc, 'predG')))
 np.save(saveFileBase+'.npy', m)
-np.savetxt(saveFileBase+'.txt', m)
+np.savetxt(saveFileBase, m)
 
 print("Discovered matrix:\n", m)
